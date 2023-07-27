64b86010be1c90bb7c2c21e14f46ee200f9a420d
张张 <2452881636@qq.com>
2023-07-27 21:52:56 +0800

    feat(Divider)

---
+++ orange-ui/src/Divider/index.md
@@ -0,0 +1,21 @@
+---
+title: Divider 分割线
+nav:
+  title: 通用
+  path: /component
+group:
+  path: /component
+---
+
+#  Divider 分割线
+
+## 基本使用
+<code src="./demo/index1.tsx"></code>
+
+## 自定义添加文字
+<code src="./demo/index2.tsx"></code>
+
+## 垂直分割
+ <code src="./demo/index3.tsx"></code>
+ 
+<API></API>
---
+++ orange-ui/src/Divider/interface.ts
@@ -0,0 +1,20 @@
+export interface DividerProps{
+    /**
+     * @description 设置分割线方向
+     * @default horizontal
+     */
+     direction?:String;
+    /**
+     * @description 设置分割线文案的位置
+     * @default center
+     */
+     contentPosition?: String;
+
+
+}
+
+export interface DividerStyle{
+    direction?:String;
+    contentPosition?: String;
+    
+}

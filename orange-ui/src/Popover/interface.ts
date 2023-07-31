624ed1442af9c0f7e90493e2c22cb65b1b20deb4
username_Lx <2308483480@qq.com>
2023-07-31 22:13:56 +0800

    refactor:(src/Popover) 重构了部分代码

--- orange-ui/src/Popover/interface.ts
+++ orange-ui/src/Popover/interface.ts
@@ -1,43 +1,44 @@
+import React from "react";
+
 export interface PopoverProps {
-    /**
-     * @description 触发方式
-     * @default click
-     */
-    trigger?:String;
+  /**
+   * @description 触发方式
+   * @default click
+   */
+  trigger?: String;
 
-    /**
-     * @description 弹出框标题
-     */
-    title?:String;
+  /**
+   * @description 弹出框标题
+   */
+  title?: String;
 
-    /**
-     * @description 弹出框中的内容
-     */
-    content?:String;
-    
-    /**
-     * @description 宽度
-     * @default 最小宽度150px
-     */
-    width?: Number;
+  /**
+   * @description 弹出框中的内容
+   */
+  content?: String;
 
-    /**
-     * @description 弹框出现位置
-     * @default top
-     */
-    placement?:String;
+  /**
+   * @description 宽度
+   * @default 最小宽度150px
+   */
+  width?: Number;
+
+  /**
+   * @description 弹框出现位置
+   * @default top
+   */
+  placement?: String;
+  children: React.ReactNode
 }
 
-export interface PopoverStyle{
-    trigger?:String;
-    title?:any;
-    content?:any;
-    width?: String;
-    placement?:String;
-    position?:String;
-    display?:String
-    
-    
-    
+export interface PopoverStyle {
+  trigger?: String;
+  title?: any;
+  content?: any;
+  width?: String;
+  placement?: String;
+  position?: String;
+  display?: String
+
 
 }
